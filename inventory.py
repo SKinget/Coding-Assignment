f2a9ebf10ec7c78e24e9799d652e380006bbd1d6
Science Kinget <149725596+SKinget@users.noreply.github.com>
2023-11-17 17:40:01 -0600

    Add files via upload

---
+++ inventory.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+class inventory:
+    def __init__(self):
+        self.databaseName = ""
+        self.tableName = ""
+        
+    def setDatabaseName(self, databaseName):
+        self.databaseName = databaseName
+        
+    def settableName(self, tableName):
+        self.tableName = tableName
+        
+    def viewInventory():
+        
+    def searchInventory():
+
+    def decreaseStock(ISBN):
+        
