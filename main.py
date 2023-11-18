899bebd4cdad06c77a5359fba1f918515bd68b9d
Science Kinget <149725596+SKinget@users.noreply.github.com>
2023-11-17 19:54:22 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+
+#main
+
+menuOption = 1
+
+print("Menu\n")
+if (menuOption == 1):
+    print("1. Login\n2. Create Account\n3. Logout")
+    choice1 = input("Enter choice: ")
+    
+    username = input("Enter Username: ")
+    password = input("\nEnter Password: ")
