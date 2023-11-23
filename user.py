6ced4b05d472f444ab039ce1251b8fc997044f04
dustycain <149711096+dustycain@users.noreply.github.com>
2023-11-23 17:06:01 -0600

    Add files via upload
    edited the view account information function, not sure if I'm done with it yet, still working on a temporary file meant for testing the user class

--- user.py
+++ user.py
@@ -6,6 +6,9 @@ class User:
         self.tableName = tableName
         self.userID = ''
         self.loggedIn = False
+        self.conn = sqlite3.connect(self.databaseName)
+        self.cursor = self.conn.cursor()
+        
         
     def login(self):
         username = input("Username: ")
@@ -18,8 +21,20 @@ class User:
         self.loggedIn = False
         return False
         
-    def viewAccountInformation)self):
-        pass
+    def viewAccountInformation(self):
+        if not self.loggedIn:
+            print("You are not logged in.")
+            return
+        
+        self.cursor.excecute(f"SELECT * FROM {self.tableName} WHERE userID = ?", (self.userID))
+        user_data = self.cursor.fetchone()
+        
+        if user_data is None:
+            print("No data found for this user.")
+        else:
+            print("User Information:")
+            for column, value in zip(self.cursor.description, user_data):
+                print(f"{column[0]}: {value}")
     
     def createAccount(self):
         pass
